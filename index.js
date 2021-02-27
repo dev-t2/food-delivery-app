3888015321e8ffcf3261b797a930de820c82b3b7
t2 <oo_ri@naver.com>
2021-02-27 17:13:38 +0900

    GraphQL Mutation Update

--- index.js
+++ index.js
@@ -4,11 +4,25 @@ const typeDefs = `
   type Query {
     totalPhotos: Int!
   }
+
+  type Mutation {
+    postPhoto(name: String!, description: String): Boolean!
+  }
 `;
+
+let photos = [];
 
 const resolvers = {
   Query: {
-    totalPhotos: () => 42,
+    totalPhotos: () => photos.length,
+  },
+
+  Mutation: {
+    postPhoto(parent, args) {
+      photos.push(args);
+
+      return true;
+    },
   },
 };
 
