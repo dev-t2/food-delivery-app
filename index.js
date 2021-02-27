3f87bef2755f30517628cd79c5a8aaa5558d8983
t2 <oo_ri@naver.com>
2021-02-27 18:32:15 +0900

    Enum and Input Type Update

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const { ApolloServer } = require('apollo-server');
 
 const typeDefs = `
+  enum PhotoCategory {
+    SELFIE
+    PORTRAIT
+    ACTION
+    LANDSCAPE
+    GRAPHIC
+  }
+
   type Photo {
     id: ID!
     url: String!
     name: String!
     description: String
+    category: PhotoCategory!
   }
 
   type Query {
@@ -13,8 +22,14 @@ const typeDefs = `
     allPhotos: [Photo!]!
   }
 
+  input PostPhotoInput {
+    name: String!
+    description: String
+    category: PhotoCategory = PORTRAIT
+  }
+
   type Mutation {
-    postPhoto(name: String!, description: String): Photo!
+    postPhoto(input: PostPhotoInput!): Photo!
   }
 `;
 
@@ -22,6 +37,8 @@ let _id = 0;
 let photos = [];
 
 const resolvers = {
+  Photo: { url: (parent) => `http://yoursite.com/img/${parent.id}` },
+
   Query: {
     totalPhotos: () => photos.length,
     allPhotos: () => photos,
@@ -29,7 +46,7 @@ const resolvers = {
 
   Mutation: {
     postPhoto(parent, args) {
-      const createPhoto = { id: _id++, ...args };
+      const createPhoto = { id: _id++, ...args.input };
 
       photos.push(createPhoto);
 
