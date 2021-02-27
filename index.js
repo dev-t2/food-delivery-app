9754536d51e4473eba9502b05e9121f71cd9d0c5
t2 <oo_ri@naver.com>
2021-02-27 19:00:57 +0900

    One to Many Relationship Update

--- index.js
+++ index.js
@@ -9,12 +9,26 @@ const typeDefs = `
     GRAPHIC
   }
 
-  type Photo {
-    id: ID!
-    url: String!
+  input PostPhotoInput {
     name: String!
     description: String
+    category: PhotoCategory = PORTRAIT
+  }
+
+  type User {
+    id: ID!
+    name: String!
+    avatar: String
+    postedPhotos: [Photo!]!
+  }
+
+  type Photo {
+    id: ID!
+    name: String!
+    url: String!
+    description: String
     category: PhotoCategory!
+    postedBy: User!
   }
 
   type Query {
@@ -22,22 +36,51 @@ const typeDefs = `
     allPhotos: [Photo!]!
   }
 
-  input PostPhotoInput {
-    name: String!
-    description: String
-    category: PhotoCategory = PORTRAIT
-  }
-
   type Mutation {
     postPhoto(input: PostPhotoInput!): Photo!
   }
 `;
 
+const users = [
+  { id: '6dc1e629-c513-4434-8b1c-c3231a7258c3', name: 'Brunei' },
+  { id: '70653fa7-17f4-4133-8e85-e0c6ba032b07', name: 'Hawaii' },
+  { id: '68bc16dd-8b91-48f6-b757-795023c8a28f', name: 'Roads' },
+];
+
+const photos = [
+  {
+    id: '0',
+    user: '70653fa7-17f4-4133-8e85-e0c6ba032b07',
+    name: 'Ergonomic',
+    description: 'Washington port synthesize',
+    category: 'ACTION',
+  },
+  {
+    id: '1',
+    user: '68bc16dd-8b91-48f6-b757-795023c8a28f',
+    name: 'paradigms',
+    category: 'SELFIE',
+  },
+  {
+    id: '2',
+    user: '68bc16dd-8b91-48f6-b757-795023c8a28f',
+    name: 'archive',
+    description: 'SSL bus',
+    category: 'LANDSCAPE',
+  },
+];
+
 let _id = 0;
-let photos = [];
 
 const resolvers = {
-  Photo: { url: (parent) => `http://yoursite.com/img/${parent.id}` },
+  User: {
+    postedPhotos: (parent) => photos.filter((photo) => photo.user === parent.id),
+  },
+
+  Photo: {
+    url: (parent) => `http://yoursite.com/img/${parent.id}`,
+    postedBy: (parent) => users.find((user) => user.id === parent.user),
+  },
 
   Query: {
     totalPhotos: () => photos.length,
