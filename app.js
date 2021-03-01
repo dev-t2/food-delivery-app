1039a32aa4ebfeb3d82fc90bca84331684f56690
t2 <oo_ri@naver.com>
2021-03-01 22:17:31 +0900

    PlayGround Package Delete

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const dotenv = require('dotenv');
 const hpp = require('hpp');
 const helmet = require('helmet');
-const csp = require('helmet-csp');
 const cors = require('cors');
 const morgan = require('morgan');
 const express = require('express');
 const { MongoClient } = require('mongodb');
 const { ApolloServer } = require('apollo-server-express');
-const expressPlayground = require('graphql-playground-middleware-express').default;
 
 const { typeDefs, resolvers } = require('./graphql');
 
@@ -41,26 +39,13 @@ const start = async () => {
   app.use(hpp());
   app.use(helmet());
   app.use(
-    csp({
-      directives: {
-        defaultSrc: ["'self'"],
-        styleSrc: ["'self'", "'unsafe-inline'"],
-        styleSrcElem: ["'self'", 'fonts.googleapis.com', 'cdn.jsdelivr.net', "'unsafe-inline'"],
-        imgSrc: ["'self'", 'cdn.jsdelivr.net'],
-        scriptSrcElem: ["'self'", 'cdn.jsdelivr.net', "'unsafe-inline'"],
-        fontSrc: ["'self'", "'unsafe-inline'", 'fonts.gstatic.com'],
-      },
-    })
-  );
-  app.use(
     cors({
       origin: true,
     })
   );
-  app.use(morgan('combined'));
+  app.use(morgan('dev'));
 
   app.get('/', (req, res) => res.end('Welcome to the PhotoShare API by T2'));
-  app.get('/playground', expressPlayground({ endpoint: 'graphql' }));
 
   app.listen({ port: PORT }, () =>
     console.log(`GraphQL Server running @ http://localhost:${PORT}`)
