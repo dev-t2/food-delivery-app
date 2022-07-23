15ef390ae482f9107db8b27b7dc3027afb1d7b75
t2 <oo_ri@naver.com>
2022-07-23 22:28:43 +0900

    food-delivery Repository Reset

--- graphql/index.js
+++
@@ -1,2 +0,0 @@
-exports.typeDefs = require('./typeDefs');
-exports.resolvers = require('./resolvers');
--- graphql/resolvers/Query.js
+++
@@ -1,7 +0,0 @@
-const me = (parent, args, { currentUser }) => currentUser;
-const totalUsers = (parent, args, { db }) => db.collection('users').estimatedDocumentCount();
-const allUsers = (parent, args, { db }) => db.collection('users').find().toArray();
-const totalPhotos = (parent, args, { db }) => db.collection('photos').estimatedDocumentCount();
-const allPhotos = (parent, args, { db }) => db.collection('photos').find().toArray();
-
-module.exports = { me, totalUsers, allUsers, totalPhotos, allPhotos };
