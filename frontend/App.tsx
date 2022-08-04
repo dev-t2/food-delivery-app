439623b50f4a7ff32a9a356c941ba43fa152eebd
t2 <oo_ri@naver.com>
2022-08-04 23:45:10 +0900

    Redux Toolkit Refactoring

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,19 +1,19 @@
-import React, { memo, useState } from 'react';
+import React, { memo } from 'react';
 import { Provider } from 'react-redux';
 import { ThemeProvider } from '@emotion/react';
 import { NavigationContainer } from '@react-navigation/native';
 
-import { store } from './src/store';
+import { store, useAppSelector } from './src/store';
 import { theme } from './src/utilities/theme';
 import { Main, Sign } from './src/pages';
 
 function App() {
-  const [isLoggedIn] = useState(false);
+  const { email } = useAppSelector(state => state.user);
 
   return (
     <Provider store={store}>
       <ThemeProvider theme={theme}>
-        <NavigationContainer>{isLoggedIn ? <Main /> : <Sign />}</NavigationContainer>
+        <NavigationContainer>{email ? <Main /> : <Sign />}</NavigationContainer>
       </ThemeProvider>
     </Provider>
   );
--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -18,8 +18,8 @@ export const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>;
 
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
 export type AppDispatch = typeof store.dispatch;
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
-
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
