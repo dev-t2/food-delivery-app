103b65cea14647f7e234040c23a04ad45b7f094f
t2 <oo_ri@naver.com>
2022-08-04 23:38:44 +0900

    Redux Toolkit Store Update

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -21,4 +21,5 @@ export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
+
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
