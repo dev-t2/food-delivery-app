5d998d56c48c1d3927293ae48830cdd9685c5010
t2 <oo_ri@naver.com>
2022-08-04 23:35:39 +0900

    Redux Toolkit Store Update

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -16,12 +16,9 @@ export const store = configureStore({
   },
 });
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
 
-// Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
