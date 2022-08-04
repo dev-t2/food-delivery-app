2659cdcd4018137d3790bb335f43e0ea846323f9
t2 <oo_ri@naver.com>
2022-08-04 21:31:38 +0900

    Redux Toolkit Store Middleware Redux Flipper Setup

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -4,6 +4,15 @@ import rootReducer from '../slices';
 
 export const store = configureStore({
   reducer: rootReducer,
+  middleware: getDefaultMiddleware => {
+    if (__DEV__) {
+      const createDebugger = require('redux-flipper').default;
+
+      return getDefaultMiddleware().concat(createDebugger());
+    }
+
+    return getDefaultMiddleware();
+  },
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
