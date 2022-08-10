5998aecd055c688972be38d5de22daaa57adca5f
t2 <oo_ri@naver.com>
2022-08-11 00:03:14 +0900

    Redux Toolkit Query User API Update

--- frontend/src/slices/userApi.ts
+++ frontend/src/slices/userApi.ts
@@ -1,15 +1,17 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+interface ICreateUser {
+  email: string;
+  name: string;
+  password: string;
+}
+
 const userApi = createApi({
   reducerPath: 'userApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8080/' }),
   endpoints: builder => ({
-    createUser: builder.mutation<void, void>({
-      query: () => ({
-        url: '/',
-        method: 'POST',
-        body: {},
-      }),
+    createUser: builder.mutation<void, ICreateUser>({
+      query: body => ({ url: 'user', method: 'POST', body }),
     }),
   }),
 });
