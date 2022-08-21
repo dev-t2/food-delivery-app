ec5fd66bf479bbdc05177d192aacd64bed234fae
t2 <oo_ri@naver.com>
2022-08-22 00:00:07 +0900

    React Native Config BASE_URL Setup

--- frontend/src/slices/index.ts
+++ frontend/src/slices/index.ts
@@ -1,11 +1,12 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import Config from 'react-native-config';
 
 import userSlice from './userSlice';
 
 export const rootApi = createApi({
   reducerPath: 'rootApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8080/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: Config.BASE_URL }),
   endpoints: () => ({}),
 });
 
