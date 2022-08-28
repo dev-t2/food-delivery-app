d8e0672882c2dd49e4d5bb0aba97746451f3bcea
t2 <oo_ri@naver.com>
2022-08-29 00:05:33 +0900

    Order API Socket IO Setup

--- react-native/src/slices/order/orderApi.ts
+++ react-native/src/slices/order/orderApi.ts
@@ -1,7 +1,27 @@
+import { io } from 'socket.io-client';
+import Config from 'react-native-config';
+
 import { api } from '../index';
 
 const orderApi = api.injectEndpoints({
-  endpoints: () => ({}),
+  endpoints: builder => ({
+    getOrders: builder.query({
+      query: () => '/socket.io',
+      async onCacheEntryAdded(_, { cacheDataLoaded, cacheEntryRemoved }) {
+        const socket = io(Config.BASE_URL);
+
+        try {
+          await cacheDataLoaded;
+        } catch (error) {
+          console.error(error);
+        } finally {
+          await cacheEntryRemoved;
+
+          socket.close();
+        }
+      },
+    }),
+  }),
   overrideExisting: __DEV__,
 });
 
