1a907c3f10e2f5933b255783d93941a58f7445f1
t2 <oo_ri@naver.com>
2022-08-29 23:24:46 +0900

    Redux Toolkit Query Order API Update

--- react-native/src/slices/order/orderApi.ts
+++ react-native/src/slices/order/orderApi.ts
@@ -1,13 +1,19 @@
+import { createEntityAdapter, EntityState } from '@reduxjs/toolkit';
 import Config from 'react-native-config';
 import { io } from 'socket.io-client';
 
 import { api } from '../index';
 import { IOrder } from './orderType';
 
+const ordersAdapter = createEntityAdapter<IOrder>();
+
 const orderApi = api.injectEndpoints({
   endpoints: builder => ({
-    getOrders: builder.query<IOrder, void>({
+    getOrders: builder.query<EntityState<IOrder>, void>({
       query: () => 'orders',
+      transformResponse(response: { data: IOrder[] }) {
+        return ordersAdapter.addMany(ordersAdapter.getInitialState(), response.data);
+      },
       async onCacheEntryAdded(_, { cacheDataLoaded, cacheEntryRemoved }) {
         const socket = io(Config.BASE_URL, { transports: ['websocket'] });
 
